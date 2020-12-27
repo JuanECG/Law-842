7104c6bb1cb15ff20631dc362e05516d3c7edf15
Juanes <juancastro251@gmail.com>
2020-12-26 19:59:54 -0500

    base components implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "antd/dist/antd.css";
+import { useState} from "react";
+import { Button } from 'antd';
+import { PlusCircleOutlined } from '@ant-design/icons';
+import TopNavbar from './components/TopNavbar';
+import Filter from './components/search';
+import Element from './components/element';
+import './styles/App.css';
 
-function App() {
+const App = () => {
+
+  const dummy = [
+    {
+      id: '1',
+      type: 'título',
+      
+      title: 'GENERALIDADES',
+      child: [
+        {
+          id: '1',
+          type: 'capítulo',
+      
+          title: 'DEFINICIÓN Y ALCANCES',
+          child: [
+            {
+              id: 1,
+              type: 'artículo',
+      
+              title: 'CONCEPTO DE INGENIERÍA',
+              content: 'Se entiende por ingeniería toda aplicación de las ciencias físicas, químicas y matemáticas; de la técnica industrial y en general, del ingenio humano, a la utilización e invención sobre la materia.'
+            }
+          ]
+        }
+
+      ]
+    },
+    {
+      id: '2',
+      type: 'título',
+      
+      title: 'GENERALIDADES',
+      child: [
+        {
+          id: '1',
+          type: 'capítulo',
+          
+          title: 'DEFINICIÓN Y ALCANCES',
+          child: [
+            {
+              id: 1,
+              type: 'artículo',
+          
+              title: 'CONCEPTO DE INGENIERÍA',
+              content: ' Se entiende por ingeniería toda aplicación de las ciencias físicas, químicas y matemáticas; de la técnica industrial y en general, del ingenio humano, a la utilización e invención sobre la materia.'
+            }
+          ]
+        }
+
+      ]
+    },
+    {
+      id: 3,
+      type: 'artículo',
+      
+      title: 'CONCEPTO DE INGENIERÍA',
+      content: ' Se entiende por ingeniería toda aplicación de las ciencias físicas, químicas y matemáticas; de la técnica industrial y en general, del ingenio humano, a la utilización e invención sobre la materia.',
+    }
+  ]
+
+  const [data, setData] = useState(dummy);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TopNavbar />
+      <Filter />
+      <div className="content">
+
+        <Button
+          type="primary"
+          shape="round"
+          className="add"
+          icon={<PlusCircleOutlined />} >
+          Agregar Componentes
+        </Button>
+
+
+        {
+        data.map(data=> (
+          <Element 
+          type={data.type} 
+          title={data.title} 
+          id={data.id}
+          number={data.number}
+          data={data}/>
+        ))}
+
+      </div>
     </div>
   );
 }
