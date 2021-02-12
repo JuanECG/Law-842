4e644fbd6f42ac66c31f16078f1d9c0fe51e7bd1
Dark-Light-20 <pazsebas@hotmail.com>
2021-02-12 17:49:29 -0500

    Added MD5 file name in uploadFile

--- api/server/uploadFile.js
+++ api/server/uploadFile.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const multer = require('multer');
+const md5 = require('md5');
 // Multer definition
 const storage = multer.diskStorage({
   destination: (req, file, cb) =>
     cb(null, path.join(__dirname, '../../public/media')),
   filename: (req, file, cb) =>
-    cb(null, new Date().toISOString() + '-' + file.originalname)
+    cb(
+      null,
+      md5(new Date() + file.originalname) +
+        '.' +
+        file.originalname.split('.').pop()
+    )
 });
 const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
@@ -20,6 +26,7 @@ const upload = multer({
 
 module.exports = (req, res, next) => {
   upload.single('media')(req, res, (err) => {
+    console.log(req.file);
     if (err instanceof multer.MulterError) {
       return res
         .status(500)
